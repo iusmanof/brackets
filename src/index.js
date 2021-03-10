eec6383be38876438cde4a728de9449d307699b4
iusmanof <74846667+iusmanof@users.noreply.github.com>
2021-03-10 09:16:26 +0300

    feat: add brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const holder = [];
+  const open = [];
+  const close = [];
+  bracketsConfig.forEach((e) => {
+    open.push(e[0]);
+    close.push(e[1]);
+  });
+
+  for (let i of str) {
+    // loop trought all letters of expr
+    if (holder.includes(i) && close.includes(i)) {
+      // if  similar
+      holder.splice(-1, 1);
+    } else if (open.includes(i)) {
+      // if its oppening bracket
+      holder.push(i);
+    } else if (close.includes(i)) {
+      // if its closing
+      const openPair = open[close.indexOf(i)]; // find his pair
+      if (holder[holder.length - 1] === openPair) {
+        // check if that pair is last element in array
+        holder.splice(-1, 1); //if so, remove it
+      } else {
+        // if its not
+        holder.push(i);
+        break; // exit loop
+      }
+    }
+  }
+  return holder.length === 0; // return true if length is 0, otherwise false
+};
